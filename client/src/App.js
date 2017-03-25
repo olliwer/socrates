070aa1ebd4d1539a68ba28b2396283e3b5353777
Olli Rissanen <olli.rissanen@futurice.com>
2017-03-25 19:28:40 +0200

    Add Footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import PurchaseContainer from './components/PurchaseContainer'
 import LargePicture from './components/LargePicture'
 import PictureRow from './components/PictureRow'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <PurchaseContainer/>
         <LargePicture picture={{"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1}}/>
         <PictureRow/>
+        <Footer/>
       </div>
     );
   }
