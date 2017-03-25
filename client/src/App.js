317ecf5c66718aa389267c60de861530ffeef006
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-03-25 18:30:21 +0200

    add PictureRow component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PurchaseContainer from './components/PurchaseContainer'
+import PictureRow from './components/PictureRow'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <PurchaseContainer/>
+        <PictureRow/>
       </div>
     );
   }
