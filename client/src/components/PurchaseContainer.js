f003af9a078af7ae7e7e52de212fcedae2993aca
Olli Rissanen <olli.rissanen@futurice.com>
2017-03-25 17:54:00 +0200

    Add react components

---
+++ client/src/components/PurchaseContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class PurchaseContainer extends React.Component {
+  render() {
+    return (<ul><li>"ASD"</li></ul>);
+   	//TODO: populate list with selectors
+  }
+}
