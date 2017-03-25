8c06f99efe680b715407b1040ae5bb4744f02412
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-03-25 18:46:19 +0200

    add SmallPicture component

--- client/src/components/PictureRow.js
+++ client/src/components/PictureRow.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import SmallPicture from './SmallPicture'
 
 export default class PictureRow extends React.Component {
   render() {
     const pictures = [
-      {"name": "anttila", "path": "/static/media/uploads/anttila.png", "owner": 1},
-      {"name": "anttila", "path": "/static/media/uploads/anttila.png", "owner": 1},
-      {"name": "anttila", "path": "/static/media/uploads/anttila.png", "owner": 1},
-      {"name": "anttila", "path": "/static/media/uploads/anttila.png", "owner": 1},
-      {"name": "anttila", "path": "/static/media/uploads/anttila.png", "owner": 1}
+      {"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1},
+      {"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1},
+      {"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1},
+      {"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1},
+      {"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1},
     ]
     return (
       <div>
         {pictures.map((picture, index) => (
-          <div key={index}>{picture.name}</div>
+          <SmallPicture key={index} picture={picture} />
         ))}
       </div>
     );
