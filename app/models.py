a78bd99969c532baed1403d396f35541d128fcd0
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-03-25 15:09:10 +0200

    added Picture model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Picture(models.Model):
+    path = models.FileField(upload_to='uploads/')
+    uploaded = models.DateTimeField(default=timezone.now)
