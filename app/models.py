144ea1a44551706a53e0e053939288a0682b8988
Olli Rissanen <olli.rissanen@futurice.com>
2017-03-25 15:18:43 +0200

    Add rest of the models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Picture(models.Model):
-    path = models.FileField(upload_to='uploads/')
+    name = models.CharField(max_length=30)
+    path = models.FileField(upload_to='uploads')
+    owner = models.ForeignKey(User)
     uploaded = models.DateTimeField(default=timezone.now)
+
+class Vote(models.Model):
+    picture = models.ForeignKey(Picture)
+    user = models.ForeignKey(User)
+    time = models.DateTimeField(default=timezone.now)
+
+class Order(models.Model):
+    picture = models.ForeignKey(Picture)
+    time = models.DateTimeField(default=timezone.now)
+    contact = models.CharField(max_length=60)
+    user = models.ForeignKey(User)
