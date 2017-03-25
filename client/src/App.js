3f168231a11aed79f856cf9583534260573757a4
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-03-25 21:19:20 +0200

    add google login button

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import PurchaseContainer from './components/PurchaseContainer'
 import LargePicture from './components/LargePicture'
 import PictureRow from './components/PictureRow'
 import Footer from './components/Footer'
+import Login from './components/Login'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login/>
         <PurchaseContainer/>
         <LargePicture picture={{"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1}}/>
         <PictureRow/>
---
+++ client/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login'
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <GoogleLogin
+        clientId="186360596290-4cnh2lg5icu20cg6v37g1558r2ds3ve0.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+    );
+  }
+}
