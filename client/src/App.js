7abdf5b463dc55c4b88f37abbf1708cb64007eed
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-03-25 19:02:34 +0200

    add LargePicture component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PurchaseContainer from './components/PurchaseContainer'
+import LargePicture from './components/LargePicture'
 import PictureRow from './components/PictureRow'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <PurchaseContainer/>
+        <LargePicture picture={{"name": "anttila", "path": "http://127.0.0.1:9000/static/media/uploads/anttila.png", "owner": 1}}/>
         <PictureRow/>
       </div>
     );
