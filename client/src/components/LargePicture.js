6e58d079a46368a98095a29420a3b73c51150380
Olli Rissanen <olli.rissanen@futurice.com>
2017-03-25 19:23:26 +0200

    Add fonts & minor css changes

--- client/src/components/LargePicture.js
+++ client/src/components/LargePicture.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class LargePicture extends React.Component {
   render() {
     return (
-      <div>
+      <div className="largePicture">
         <img alt={this.props.picture.name} src={this.props.picture.path}/>
       </div>
     );
